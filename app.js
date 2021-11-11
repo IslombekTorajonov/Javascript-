5b9921eac650d00f7a6dfce6f81b67ad4f17795e
Islombek <90696696+torajonovislombek@users.noreply.github.com>
2021-11-11 13:02:06 +0500

    vue js
    js

--- app.js
+++ app.js
@@ -1,36 +1,51 @@
-
-// const AppHowHide = {
-// 	data(){
-// 		return{
-// 			word:''
-// 		}
-// 	}
-// Vue.create(AppHowHide).mount('#app')
 const App = {
-	data(){
-		return{
-			color:"blue",
-			bg:"orange",
-			status:false,
-			
-			styles:{
-				fontSize:"30px",
-				borderRadius:"10px",
-				background:"orange",
-				padding:"5px",
-			},
-			styleX:{
-				fontSize:"20px",
-				borderRadius:"10px",
-				background:"red",
-				padding:"5px",	
-			}
-		}
-	},
-	methods:{
-	change(){
-		this.bg = "red"
-	}
+    data(){
+        return {
+            name:"John",
+            surname:"Doe",
+            message:'No comment',
+            status:false,
+            bg:"orange",
+            styles:{
+                fontSize:"30px",
+                borderRadius:"10px",
+                background:"orange",
+                padding:"5px"
+            },
+            stylesX:{
+                fontSize:"20px",
+                borderRadius:"10px",
+                background:"red",
+                padding:"5px"
+            },
+            box:{
+                padding: "10px",
+                borderRadius:"10px",
+                background:"lightblue",
+            },
+            fonts:{
+                fontSize:"30px",
+                fontWeight:"700",
+                textAlign:"center"
+            }
+        }
+    },
+    methods:{
+        change(){
+           // this.bg = "red"
+           //  this.check = true
+        },
+
+    },
+    computed:{
+        reversedMessage() {
+            return this.message
+                .split('')
+                .reverse().join('');
+        },
+        fullName(){
+            return `${this.name}-${this.surname}`
+        }
+    }
 }
-},
 Vue.createApp(App).mount("#app")
