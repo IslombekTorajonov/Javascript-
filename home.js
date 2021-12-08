1df00ad54af031538a07dc83edd4599a1070ce6f
developer <90696696+IslombekTorajonov@users.noreply.github.com>
2021-12-08 18:56:36 +0500

    forms js

---
+++ home.js
@@ -0,0 +1,20 @@
+
+                       //  LESSON 2
+const App = {
+	data(){
+		return{
+			seen:1,
+			people:[
+			{name:'Hayotbek'}
+			{name:'John'}
+			{name:'Doe'}] 
+		}
+	}
+	}
+	Vue.createApp(App).mount('#app')
+		
+	const app = Vue.createApp(MyApp)
+	app.copponent("list-item",{
+		template:`<form action></form>`
+	})
+app.mount("#app")
